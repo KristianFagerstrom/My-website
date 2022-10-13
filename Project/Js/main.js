fc3e42589a79e0ed937e55f9741b911e2b0f9866
KristianFagerstrom <37860635+KristianFagerstrom@users.noreply.github.com>
2022-10-13 17:39:45 +0300

    Creating main js

---
+++ Project/Js/main.js
@@ -0,0 +1,10 @@
+function clickCounter() {
+     
+        if (localStorage.clickcount) {
+            localStorage.clickcount++;
+        } else {
+            localStorage.setItem('clickcount', 1);
+           // localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    } 
