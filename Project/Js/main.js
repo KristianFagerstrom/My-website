81be000a9433175bc0d8cb59a399d145c6067aff
KristianFagerstrom <37860635+KristianFagerstrom@users.noreply.github.com>
2022-10-25 20:04:12 +0300

    Js added and made local storage
    Buttons aligned.  Not responsive yet.
    New font family

--- Project/Js/main.js
+++ Project/Js/main.js
@@ -1,12 +1,39 @@
-function clickCounter() {
-     
-        if (localStorage.clickcount) {
-            localStorage.clickcount++;
-        } else {
-            localStorage.setItem('clickcount', 1);
-           // localStorage.clickcount = 1;
-        }
-        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
-    } 
+function addUIItem(txt) {
+  let li = document.createElement("li");
+  li.innerHTML = txt;
+  list.insertBefore(li, list.childNodes[0]);
+  const dldbutton = document.createElement("button");
+  dldbutton.textContent = "x";
+  dldbutton.classList.add("fas", "fa-trash-alt");
+  li.appendChild(dldbutton);
+  dldbutton.addEventListener("click", (e) => {
+    li.parentNode.removeChild(li);
+    savedTasks = savedTasks.filter((e) => e !== txt);
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+  });
+}
+let input = document.querySelector("#tdn");
+let btn = document.querySelector("#add");
+let list = document.querySelector("#list");
+// load saved tasts
+let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// add UI elements for any saved task
+savedTasks.forEach(addUIItem);
 
+add.addEventListener("click", () => {
+  let txt = input.value;
+  if (txt === "") {
+    alert("Please write something to do and find the easter egg in the site");
+  } else {
+    savedTasks.push(txt);
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+    input.value = "";
+    addUIItem(txt);
+  }
+});
 
+list.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("checked");
+  }
+});
