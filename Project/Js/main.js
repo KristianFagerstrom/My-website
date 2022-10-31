a955946e1d3eb5cb6e102d9ad484d021566fe5be
KristianFagerstrom <37860635+KristianFagerstrom@users.noreply.github.com>
2022-10-31 16:28:46 +0200

    new js and better optimized for mobile

--- Project/Js/main.js
+++ Project/Js/main.js
@@ -1,39 +1,74 @@
-function addUIItem(txt) {
-  let li = document.createElement("li");
-  li.innerHTML = txt;
-  list.insertBefore(li, list.childNodes[0]);
-  const dldbutton = document.createElement("button");
-  dldbutton.textContent = "x";
-  dldbutton.classList.add("fas", "fa-trash-alt");
-  li.appendChild(dldbutton);
-  dldbutton.addEventListener("click", (e) => {
-    li.parentNode.removeChild(li);
-    savedTasks = savedTasks.filter((e) => e !== txt);
-    localStorage.setItem("tasks", JSON.stringify(savedTasks));
-  });
-}
-let input = document.querySelector("#tdn");
-let btn = document.querySelector("#add");
-let list = document.querySelector("#list");
-// load saved tasts
-let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-// add UI elements for any saved task
-savedTasks.forEach(addUIItem);
+window.addEventListener("load", () => {
+  const form = document.querySelector("#task-form");
+  const input = document.querySelector("#tdn");
+  const list = document.querySelector("#tasks");
 
-add.addEventListener("click", () => {
-  let txt = input.value;
-  if (txt === "") {
-    alert("Please write something to do and find the easter egg in the site");
-  } else {
-    savedTasks.push(txt);
-    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const task = input.value;
+    if (!task) {
+      return;
+    }
+
+    const task_div = document.createElement("div");
+    task_div.classList.add("task");
+    list.appendChild(task_div);
+
+    const task_content_div = document.createElement("div");
+    task_content_div.classList.add("content");
+    task_div.appendChild(task_content_div);
+
+    const task_input = document.createElement("input");
+    task_input.classList.add("text");
+    task_input.type = "text";
+    task_input.value = task;
+    task_input.setAttribute("readonly", "readonly");
+    task_content_div.appendChild(task_input);
+
+    const task_actions_div = document.createElement("div");
+    task_actions_div.classList.add("actions");
+    task_div.appendChild(task_actions_div);
+
+    const task_edit_botton = document.createElement("button");
+    task_edit_botton.classList.add("Edit");
+    task_edit_botton.innerHTML = "Edit";
+
+    const task_delete_button = document.createElement("button");
+    task_delete_button.classList.add("Delete");
+    task_delete_button.innerHTML = "Delete";
+
+    const task_completed_button = document.createElement("button");
+    task_completed_button.classList.add("Completed");
+    task_completed_button.innerHTML = "Completed";
+
+    task_actions_div.appendChild(task_edit_botton);
+    task_actions_div.appendChild(task_completed_button);
+    task_actions_div.appendChild(task_delete_button);
+
+    task_edit_botton.addEventListener("click", () => {
+      if (task_edit_botton.innerText.toLowerCase() == "edit") {
+        task_input.removeAttribute("readonly");
+        task_input.focus();
+        task_edit_botton.innerText = "Save";
+        task_input.style.textDecoration = "none";
+      } else {
+        task_input.setAttribute("readonly", "readonly");
+        task_edit_botton.innerText = "Edit";
+      }
+    });
+
+    task_delete_button.addEventListener("click", () => {
+      if (confirm("Are you sure you want to delete this task?")) {
+        list.removeChild(task_div);
+      }
+    });
+
+    task_completed_button.addEventListener("click", () => {
+      task_input.style.textDecoration = "line-through";
+      task_input.setAttribute("readonly", "readonly");
+    });
+
     input.value = "";
-    addUIItem(txt);
-  }
-});
-
-list.addEventListener("click", (e) => {
-  if (e.target.tagName == "LI") {
-    e.target.classList.toggle("checked");
-  }
+  });
 });
