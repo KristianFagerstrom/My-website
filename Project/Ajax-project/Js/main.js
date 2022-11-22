f5878dd443e47a61933355615f1141a6f3250482
KristianFagerstrom <37860635+KristianFagerstrom@users.noreply.github.com>
2022-11-22 20:12:55 +0200

    valitsin nappula

--- Project/Ajax-project/Js/main.js
+++ Project/Ajax-project/Js/main.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+    $("#reset").click(function (e){
+        $("#Kaupunkivalitsin").val("Valitse")
+        $("#viesti").html("")
+        
+    });
+//valitsee kaupunging nappulan avulla
 $.ajax({
     type: "POSt",
     url: "http://pro.openweathermap.org/data/2.5/weather?id="+ $("#citySelect").val() + "&appid=f6c2716e92fa61a4a94481d15d56dc9d&units=metric", 
@@ -9,3 +16,4 @@ error: function(){
     alert("error loading data")
 }
 });
+}
