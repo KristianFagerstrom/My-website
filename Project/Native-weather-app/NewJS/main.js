1a9a94dd71270ed3aaec736914691f75c23cabd1
KristianFagerstrom <37860635+KristianFagerstrom@users.noreply.github.com>
2022-11-29 14:25:15 +0200

    Made native js and changed the layout
    New index file made to fit js. Jquery layout didnt fit int native js requirements. did locally changes to website.

--- Project/Native-weather-app/NewJS/main.js
+++ Project/Native-weather-app/NewJS/main.js
@@ -1,10 +1,56 @@
+let button = document.querySelector(".nappi");
+let inputvalue = document.querySelector(".search-laatikko");
+let temp = document.querySelector(".current .temp");
+let description = document.querySelector(".current .weather");
+let city = document.querySelector(".location .kaupunki");
+let date = document.querySelector(".location .päivämäärä");
 
-// lataa sivuston funktio
+button.addEventListener("click", function () {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=92e59231142e620888c7b8222e940a6e`
+  )
+    .then((response) => response.json())
+    .then(displayData)
+    .catch((err) => alert("Please Enter Correct City Name"));
+});
 
+const displayData = (weather) => {
+  temp.innerHTML = `${Math.round(weather.main.temp)}°C`;
+  description.innerText = `${weather.weather[0].description}`;
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+  let now = new Date();
+  date.innerText = dateBuilder(now);
+};
 
-  
-  //valitsee kaupunging nappulan avulla
-  
-        // kaikki tarvittavat taulut johon syötetään tietoa
-      
-  // error funktio joka kertoo jos syötettävä asia ei ole oikein
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
